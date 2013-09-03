eea4496c8462b7d3d7205ffe225acd55f67b6ec4
Alan Busby <thebusby@thebusby.com>
2013-09-03 23:27:10 +0900

    changed project URL

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-aws-imds "1.0.0"
   :description "Clojure AWS IMDS library."
-  :url "http://clojars.org/clj-aws-imds"
+  :url "https://github.com/thebusby/clj-aws-imds"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
